e211ba1f7e0f67bc9138a3308bdc3a86fd9ea7c1
Karen Polanco <polanco.k@gmail.com>
2022-11-21 15:37:17 -0500

    commit tree 1b43bd39c8b11381c317733f5f65e86537bfab25

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(name, string) {
+    const noMatch = []
+    const match = name.filter(name => name.toLowerCase() === string.toLowerCase())
+    if(match){
+        return match
+    }
+    else return noMatch
+}
+
+function fuzzyMatch(name, string) {
+    const match = name.filter(name => name.startsWith(string))
+    return match
+}
+
+function matchName(driver, string) {
+    const match = driver.filter(driver => driver.name === string)
+    return match
+}
